d6f1dddc2faed61720f6de06137337585c9790d9
André Baceti <a.baceti@murabei.com>
2022-02-11 14:20:23 -0300

    Ajusting test xdocker-compose

--- test-aux/docker-compose/docker-versions.sh
+++ test-aux/docker-compose/docker-versions.sh
@@ -1,6 +1,6 @@
 ###########
 # Variables
 # Auth
-export TEST_DB_PUMPWOOD_AUTH="0.3"
-export PUMPWOOD_AUTH_APP="0.4"
+export TEST_DB_PUMPWOOD_AUTH="0.0"
+export PUMPWOOD_AUTH_APP="0.0"
 export PUMPWOOD_AUTH_STATIC="0.0"

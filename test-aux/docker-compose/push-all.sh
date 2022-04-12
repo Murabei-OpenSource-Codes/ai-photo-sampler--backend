f6a1a14fc1a9fd765c6547e2407a6940041709cb
André Andrade Baceti <a.baceti@murabei.com>
2022-04-11 21:21:04 -0300

    Ajusting docker versions

--- test-aux/docker-compose/push-all.sh
+++ test-aux/docker-compose/push-all.sh
@@ -1,5 +1,5 @@
 source docker-versions.sh
 
-docker push southamerica-east1-docker.pkg.dev/serene-boulder-340918/private-images/test-db-ai-photo-sampler:$TEST_DB_PUMPWOOD_AUTH
-docker push southamerica-east1-docker.pkg.dev/serene-boulder-340918/private-images/ai-photo-sampler-app:$PUMPWOOD_AUTH_APP
-docker push southamerica-east1-docker.pkg.dev/serene-boulder-340918/private-images/ai-photo-sampler-static:$PUMPWOOD_AUTH_APP
+docker push andrebaceti/test-db-ai-photo-sampler:$TEST_DB_PUMPWOOD_AUTH
+docker push andrebaceti/ai-photo-sampler-app:$PUMPWOOD_AUTH_APP
+docker push andrebaceti/ai-photo-sampler-static:$PUMPWOOD_AUTH_STATIC
